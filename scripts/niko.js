75e94ddbf2bcfbb3c96d16e6001520f2451b813c
cracktorio <mlg.nlgamer@gmail.com>
2026-05-22 22:52:58 +0200

    i just want my style bro 🤦‍♂️🤦‍♂️🤦‍♂️🤦‍♂️

--- scripts/niko.js
+++ scripts/niko.js
@@ -23,6 +23,23 @@
             });
           });
         });
+
+        BABYLON.SceneLoader.ImportMeshAsync("", "resources/", "i_just_want_my_style_bro.glb", scene).then(function(result) {
+          result.meshes.forEach(function(mesh) {
+            if (mesh.material) {
+              if (mesh.material.emissiveColor === undefined) {
+                mesh.material.emissiveColor = new BABYLON.Color3(1, 1, 1);
+              }
+              mesh.material.disableLighting = true;
+            }
+            mesh.position.y += 0.4;
+          });
+          scene.onBeforeRenderObservable.add(function() {
+            result.meshes.forEach(function(mesh) {
+              mesh.rotation.y += 0.05;
+            });
+          });
+        });
         
         engine.runRenderLoop(() => scene.render());
         window.addEventListener('resize', () => engine.resize());
